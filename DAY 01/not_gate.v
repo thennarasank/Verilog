7f635dc74923c2c0d123e36855798abc29710657
thennarasank <thennarasan.asus@gmail.com>
2025-12-09 19:50:32 +0530

    Add files via upload

---
+++ DAY 01/not_gate.v
@@ -0,0 +1,36 @@
+module not_gate
+      (output y,input a);
+assign y = ~a;
+endmodule
+
+
+//tb
+module tb_not_gate;
+reg  a;
+wire y;
+
+//dut
+not_gate dut (y,a);
+
+
+initial begin
+	$monitor ("$time=%0t |a=%b | out=%b",$time,a,y);
+$dumpfile("not_gate.vcd");
+$dumpvars();
+/*
+a=0;
+#15
+a=1;
+#10;
+a=1;
+#5;
+a=0;
+#10;*/
+repeat (10)begin
+	a=$random;
+	#5;
+end
+
+$finish;
+end
+endmodule
