54cf9203fae5432507b558b8f300f60d3adfa14d
thennarasank <thennarasan.asus@gmail.com>
2025-12-10 16:17:41 +0530

    Add files via upload

---
+++ DAY 03/16to1_demux.v
@@ -0,0 +1,31 @@
+//design
+module demux_16to1(
+input wire a,
+input wire[3:0] s,
+output reg[15:0] out);
+always @(*) begin 
+out = 16'b0;
+out [s] = a;
+end
+endmodule
+//test bench
+module tb_demux_16to1;
+reg  a;
+reg  [3:0]s;
+wire [15:0] out;
+//dut
+demux_16to1 dut
+(.a(a),.s(s),.out(out));
+initial begin
+$display("------------------------------------------------------------------");
+$monitor(" | a=%b | s=%b | out=%b |  time=%0t  | ",a,s,out,$time);
+$dumpfile("demux_16to1.vcd");
+$dumpvars();
+repeat (20)begin
+	a=1;
+	s=$random;
+	#10;
+end
+$finish;
+end
+endmodule
