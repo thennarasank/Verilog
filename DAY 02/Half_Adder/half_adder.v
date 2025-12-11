c0cce6cb9017ff916c22b3c425d09b1ae9c9c1ea
thennarasank <thennarasan.asus@gmail.com>
2025-12-11 16:16:05 +0530

    Update half_adder.v

--- DAY 02/Half_Adder/half_adder.v
+++ DAY 02/Half_Adder/half_adder.v
@@ -1,18 +1,8 @@
-//design
-
-module half_adder(input a,b,output s,c);
-xor xor_gate(s,a,b);
-and and_gate(c,a,b);
-endmodule
-
 //test_bench
-
 module half_adder_tb();
 reg a,b;
 wire s,c;
-
 //dut
-
 half_adder dut(a,b,s,c);
 initial begin
         $monitor("time=%0t,a=%b,b=%b,s=%b,c=%b",$time,a,b,s,c);
