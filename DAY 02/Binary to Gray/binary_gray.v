4c873eecc41eaddd926a25eaf166ad472293304b
thennarasank <thennarasan.asus@gmail.com>
2025-12-12 10:23:25 +0530

    Update and rename DAY 02/binary_gray.v to DAY 02/Binary to Gray/binary_gray.v

--- DAY 02/Binary to Gray/binary_gray.v
+++ DAY 02/Binary to Gray/binary_gray.v
@@ -1,10 +1,3 @@
-//design
-module binary_gray( input [3:0]binary, output [3:0]gray );
-assign gray [3] = binary [3] ;
-assign gray [2] = binary [3] ^ binary [2];
-assign gray [1] = binary [2] ^ binary [1];
-assign gray [0] = binary [1] ^ binary [0];
-endmodule
 //test bench
 module tb_binary_gray;
 reg [3:0]binary;
