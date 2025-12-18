c1196a37173efc2d46a20e0cf47151daade960fe
thennarasank <thennarasan.asus@gmail.com>
2025-12-18 09:55:56 +0530

    Update blocking.v

--- Sequential/DAY 01/BLOCKING/blocking.v
+++ Sequential/DAY 01/BLOCKING/blocking.v
@@ -1,16 +1,3 @@
-//design
-module blocking (
-output reg a,
-output reg b,
-output reg c
-);
-always @(*) begin
-a = 0;
-b = a;
-a = b;
-c = 1;
-end
-endmodule
 //TEST BENCH
 module tb_blocking;
 wire a, b, c;
