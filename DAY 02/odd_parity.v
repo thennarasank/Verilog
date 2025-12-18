2b527c0f4c0cc7e9f225a816706f90fcd69b74d5
thennarasank <thennarasan.asus@gmail.com>
2025-12-18 10:13:59 +0530

    Add files via upload

---
+++ DAY 02/odd_parity.v
@@ -0,0 +1,26 @@
+//design
+module odd_parity(input [3:0]a ,input parity,output error);
+assign error = ~(a[0]^a[1]^a[2]^a[3]^parity);
+endmodule
+//test bench
+module tb_odd_parity;
+reg [3:0]a;
+reg parity;
+wire error;
+odd_parity dut(.a(a),.parity(parity),.error(error));
+initial begin
+$display("--------------------ODD PARITY--------------------");
+$monitor("   a=%b   |   parity=%b   |   error=%b   |   time%0t   ",a,parity,error,$time);
+$dumpfile("odd_parity.vcd");
+$dumpvars();
+a=4'b0000 ; parity=0 ; #10;
+a=4'b0001 ; parity=1 ; #10;
+a=4'b0011 ; parity=0 ; #10;
+a=4'b0111 ; parity=1 ; #10;
+a=4'b1110 ; parity=0 ; #10;
+a=4'b1100 ; parity=1 ; #10;
+a=4'b1000 ; parity=0 ; #10;
+a=4'b0000 ; parity=1 ; #10;
+$finish; 
+end 
+endmodule
