544e4768643f1c8a35b01e2bfcf6b2184c98a1a9
thennarasank <thennarasan.asus@gmail.com>
2025-12-19 16:49:11 +0530

    Update and rename Sequential/Latches/jk_latch.v to Sequential/Latches/JK Latch/jk_latch.v

--- Sequential/Latches/JK Latch/jk_latch.v
+++ Sequential/Latches/JK Latch/jk_latch.v
@@ -1,16 +1,3 @@
-//design
-module jk_latch (input j,input k,input enable,output reg q);
-always @(*) begin
-if (enable) begin
-case ({j,k})
-2'b00: q = q; 
-2'b01: q = 1'b0;
-2'b10: q = 1'b1; 
-2'b11: q = ~q;     
-endcase
-end
-end
-endmodule
 //testbench
 module tb_jk_latch;
 reg j;
