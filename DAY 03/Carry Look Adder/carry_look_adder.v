50ef3f2b3387ab21159da89622127ab407b49bab
thennarasank <thennarasan.asus@gmail.com>
2025-12-19 10:43:07 +0530

    Update and rename DAY 03/carry_look_adder.v to DAY 03/Carry Look Adder/carry_look_adder.v

--- DAY 03/Carry Look Adder/carry_look_adder.v
+++ DAY 03/Carry Look Adder/carry_look_adder.v
@@ -1,18 +1,3 @@
-//design
-module carry_look_adder(input [3:0]a,input [3:0]b,input cin,output [3:0]sum,output cout);
-wire [3:0] g, p;
-wire c1, c2, c3;
-assign g = a & b;
-assign p = a ^ b;
-assign c1 = g[0] | (p[0] & cin);
-assign c2 = g[1] | (p[1] & g[0]) | (p[1] & p[0] & cin);
-assign c3 = g[2] | (p[2] & g[1]) | (p[2] & p[1] & g[0]) | (p[2] & p[1] & p[0] & cin);
-assign cout = g[3] | (p[3] & g[2]) | (p[3] & p[2] & g[1]) | (p[3] & p[2] & p[1] & g[0]) | (p[3] & p[2] & p[1] & p[0] & cin);
-assign sum[0] = p[0] ^ cin;
-assign sum[1] = p[1] ^ c1;
-assign sum[2] = p[2] ^ c2;
-assign sum[3] = p[3] ^ c3;
-endmodule
 //test bench
 module tb_carry_look_adder;
 reg [3:0]a;
