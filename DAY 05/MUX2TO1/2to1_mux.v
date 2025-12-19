280898515e8c2e8959bb7320bf07d5ea47d3e0d9
thennarasank <thennarasan.asus@gmail.com>
2025-12-19 14:41:38 +0530

    Update 2to1_mux.v

--- DAY 05/MUX2TO1/2to1_mux.v
+++ DAY 05/MUX2TO1/2to1_mux.v
@@ -1,27 +1,16 @@
-//design
-module mux_2to1 (input i0, i1, s, output y);
-    assign y = s ? i1 : i0;
-endmodule
-
 //test_bench
 module tb_mux_2to1;
 reg i0, i1, s ;
 wire y ;
-//dut
 mux_2to1 dut (i0,i1,s,y);
 initial begin
-	$monitor("time=%0t | i0=%0b | i1=%0b | s=%0b | y=%0b |",$time,i0,i1,s,y);
-	$dumpfile("tb_mux_2to1.vcd");
-	$dumpvars();
-
-	s =0; i0 =0; i1 =1;
-	#9
-	s =0 ;i0 =1; i1 =0;
-        #8
-	s =1 ;i0 =0; i1 =1;
-	#7
-	s =1 ;i0 =1; i1 =0;
-	#6
-	$finish;
+$monitor("time=%0t | i0=%0b | i1=%0b | s=%0b | y=%0b |",$time,i0,i1,s,y);
+$dumpfile("tb_mux_2to1.vcd");
+$dumpvars();
+s =0; i0 =0; i1 =1;#9
+s =0 ;i0 =1; i1 =0;#8
+s =1 ;i0 =0; i1 =1;#7
+s =1 ;i0 =1; i1 =0;#6
+$finish;
 end
 endmodule
