2e2d231f3d2bb5612779fff5e8d2e4c804287cab
thennarasank <thennarasan.asus@gmail.com>
2025-12-22 12:46:10 +0530

    Update and rename DAY 05/4to1_mux.v to DAY 05/MUX4TO1/4to1_mux.v

--- DAY 05/MUX4TO1/4to1_mux.v
+++ DAY 05/MUX4TO1/4to1_mux.v
@@ -1,7 +1,3 @@
-//design
-module mux4to1(input i0, i1, i2, i3,input s0, s1, output y);
-assign y = (~s1 & ~s0 & i0) |(~s1 &  s0 & i1) |( s1 & ~s0 & i2) |( s1 &  s0 & i3);
-endmodule
 //testbench
 module tb_mux4to1;
 reg i0, i1, i2, i3;
