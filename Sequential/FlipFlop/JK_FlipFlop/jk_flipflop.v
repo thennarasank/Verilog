7ec43589ee49655a24017e2d98164d68c6fa38a4
thennarasank <thennarasan.asus@gmail.com>
2025-12-25 23:06:21 +0530

    Update and rename jk_flipflop.v to jk_flipflop.v

--- Sequential/FlipFlop/JK_FlipFlop/jk_flipflop.v
+++ Sequential/FlipFlop/JK_FlipFlop/jk_flipflop.v
@@ -1,18 +1,3 @@
-//design
-module jk_flipflop (input clk,input reset,input j,input k,output reg q);
-always @(posedge clk or posedge reset) begin
-if (reset)
-q <= 1'b0;
-else begin
-case ({j, k})
-2'b00: q <= q;      
-2'b01: q <= 1'b0; 
-2'b10: q <= 1'b1;  
-2'b11: q <= ~q;  
-endcase
-end
-end
-endmodule
 //testbench
 module tb_jk_flipflop;
 reg clk;
