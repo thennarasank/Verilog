d240cfeaded8c827965f4a1878cb76856496d70b
thennarasank <thennarasan.asus@gmail.com>
2025-12-25 22:50:00 +0530

    Create sr_flipflop.d

---
+++ Sequential/FlipFlop/SR_FlipFlop/sr_flipflop.d
@@ -0,0 +1,11 @@
+//design
+module sr_flipflop(input clk,input s,input r,output reg q);
+always @(posedge clk) begin
+case ({s, r})
+2'b00: q <= q;     
+2'b01: q <= 1'b0;  
+2'b10: q <= 1'b1;   
+2'b11: q <= 1'bx;
+endcase
+end
+endmodule
