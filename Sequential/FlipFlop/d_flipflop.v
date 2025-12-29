07e4c29892818cc362f5ca27d5597c77de85af92
thennarasank <thennarasan.asus@gmail.com>
2025-12-29 10:31:15 +0530

    Add files via upload

---
+++ Sequential/FlipFlop/d_flipflop.v
@@ -0,0 +1,26 @@
+//design 
+module d_flipflop( input wire clk, input wire d, output reg q );
+always @(posedge clk)begin
+q <= d;
+end 
+endmodule
+//testbench
+module tb_d_flipflop;
+reg clk;
+reg d;
+wire q;
+d_flipflop dut(.d(d),.clk(clk),.q(q));
+always #10 clk = ~clk;
+initial begin
+$monitor("  d=%b  |  clk=%b  |  q=%b  |  time=%0t  ",d,clk,q,$time);
+$dumpfile("d_flipflop.vcd");
+$dumpvars();
+clk = 0; #10;
+d = 0; #10;
+d = 0; #10;
+d = 1; #10;
+d = 0; #10;
+d = 1; #10;
+$finish;
+end 
+endmodule
