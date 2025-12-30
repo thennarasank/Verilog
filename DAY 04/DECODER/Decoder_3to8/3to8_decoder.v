f86a2450ef43a671c83e43f8c9adf078813d4d42
thennarasank <thennarasan.asus@gmail.com>
2025-12-30 15:28:32 +0530

    Update 3to8_decoder.v

--- DAY 04/DECODER/Decoder_3to8/3to8_decoder.v
+++ DAY 04/DECODER/Decoder_3to8/3to8_decoder.v
@@ -1,15 +1,3 @@
-//design
-module decoder_3to8 (input [2:0] in , output[7:0] out);
-assign out[0] = ~in[2] & ~in[1] & ~in[0];
-assign out[1] = ~in[2] & ~in[1] &  in[0];
-assign out[2] = ~in[2] &  in[1] & ~in[0];
-assign out[3] = ~in[2] &  in[1] &  in[0];
-assign out[4] =  in[2] & ~in[1] & ~in[0];
-assign out[5] =  in[2] & ~in[1] &  in[0];
-assign out[6] =  in[2] &  in[1] & ~in[0];
-assign out[7] =  in[2] &  in[1] &  in[0];
-endmodule
-
 //test bench
 module tb_decoder_3to8; 
 reg[2:0] in;
