e9e06721b4b92f6d5296f1d0920fd1d53f64cf56
thennarasank <thennarasan.asus@gmail.com>
2025-12-30 16:52:57 +0530

    Update and rename down_counter.v to down_counter.v

--- Sequential/Counters/DownCounter/down_counter.v
+++ Sequential/Counters/DownCounter/down_counter.v
@@ -1,12 +1,3 @@
-//design
-module down_counter (input clk,rst,output reg [3:0]count);
-always @(posedge clk or posedge rst) begin
-if (rst)
-count <= 4'd15;
-else
-count <= count - 1;
-end
-endmodule
 //testbench
 module tb_down_counter;
 reg clk;
