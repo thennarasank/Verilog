948c3e548e29d440e5a2054bc7c9d06c263f6ef9
thennarasank <thennarasan.asus@gmail.com>
2025-12-31 11:16:46 +0530

    Update and rename DAY 05/4to1_demux.v to DAY 05/DEMUX4TO1/4to1_demux.v

--- DAY 05/DEMUX4TO1/4to1_demux.v
+++ DAY 05/DEMUX4TO1/4to1_demux.v
@@ -1,13 +1,3 @@
-//design
-module demux_4to1(
-input wire a,
-input wire[1:0] s,
-output reg[3:0] out);
-always @(*) begin 
-out = 4'b0;
-out [s] = a;
-end
-endmodule
 //test bench
 module tb_demux_4to1;
 reg  a;
