3188b16ee6685d4458f541eb313cb2536b30f2ac
thennarasank <thennarasan.asus@gmail.com>
2026-01-06 09:44:35 +0530

    Update and rename Sequential/4_bit_register.v to Sequential/Register/4Bit-Register/4_bit_register.v

--- Sequential/Register/4Bit-Register/4_bit_register.v
+++ Sequential/Register/4Bit-Register/4_bit_register.v
@@ -1,12 +1,3 @@
-//design
-module bit4_register (input wire clk,input wire rst,input wire [3:0]d,output reg [3:0]q);
-always @(posedge clk or posedge rst) begin
-if (rst)
-q <= 4'b0000;
-else
-q <= d;
-end
-endmodule
 //testbench
 module tb_bit4_register;
 reg clk;
