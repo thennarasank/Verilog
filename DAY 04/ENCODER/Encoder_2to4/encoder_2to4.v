503f285a125c41a057fef23d6a149c1e0323e67c
thennarasank <thennarasan.asus@gmail.com>
2026-01-06 10:12:23 +0530

    Update and rename encoder_2to4.v to encoder_2to4.v

--- DAY 04/ENCODER/Encoder_2to4/encoder_2to4.v
+++ DAY 04/ENCODER/Encoder_2to4/encoder_2to4.v
@@ -1,15 +1,3 @@
-//design
-module encoder_2to4(input [1:0]d,output reg [3:0]q);
-always @(*) begin
-case (d)
-2'b00: q = 4'b0001;
-2'b01: q = 4'b0010;
-2'b10: q = 4'b0100;
-2'b11: q = 4'b1000;
-default: q = 4'b0000;
-endcase
-end
-endmodule
 //testbench
 module tb_encoder_2to4;
 reg [1:0]d;
