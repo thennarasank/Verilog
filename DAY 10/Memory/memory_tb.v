a8000fbace880f9b2dd06d6b2a671cc42d86e52f
Thennarasan K <thennarasan.asus@gmail.com>
2026-03-19 10:48:59 +0530

    Update memory_tb.v

--- DAY 10/Memory/memory_tb.v
+++ DAY 10/Memory/memory_tb.v
@@ -1,17 +1,16 @@
 module tb_memory;
 reg [3:0] reg_a [0:2];
-//dut 
- memory dut();
+	 memory dut();
 initial begin
-reg_a[0] = 12;
-reg_a[1] = 10;
-reg_a[2] = 5;
-$display("reg_a[0] = %b", reg_a[0]);
-$display("reg_a[1] = %d", reg_a[1]);
-$display("reg_a[2] = %h", reg_a[2]);
-$dumpfile("memory.vcd");
-$dumpvars();
+     reg_a[0] = 12;
+     reg_a[1] = 10;
+     reg_a[2] = 5;
+   $display("reg_a[0] = %b", reg_a[0]);
+   $display("reg_a[1] = %d", reg_a[1]);
+   $display("reg_a[2] = %h", reg_a[2]);
+   $dumpfile("memory.vcd");
+   $dumpvars();
 #10;
-$finish;
-end 
+	$finish;
+	end 
 endmodule
